b445df8054f60e09d30abe7662fe84418086b7eb
Sachin Singh Thakur <0sachinthakur0@gmail.com>
2026-04-24 14:21:11 -0700

    Added database and User model

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cloudvault.db'
+
+db = SQLAlchemy(app)
+
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), default='user')  # user or admin
 
 @app.route('/')
 def home():
-    return "CloudVault is running 🚀"
+    return "DB connected!"
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
